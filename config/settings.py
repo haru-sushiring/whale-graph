b7e911fa2efbc991a748d8dc6b98f47648ed9d28
haru-sushiring <lemolemon00buroger@gmail.com>
2023-01-08 08:28:03 +0000

    DEBUG =False

--- config/settings.py
+++ config/settings.py
@@ -32,9 +32,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG =False
 
-#ALLOWED_HOSTS = ['54.92.62.67']
 ALLOWED_HOSTS = ['whale-analytics.com']
 
 
