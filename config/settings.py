1b6862b4e98dfb264c8d25c28d103b3ce6aeca0e
haru-sushiring <lemolemon00buroger@gmail.com>
2023-01-08 08:24:07 +0000

    set ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -34,6 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+#ALLOWED_HOSTS = ['54.92.62.67']
 ALLOWED_HOSTS = ['whale-analytics.com']
 
 
@@ -100,11 +101,11 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASS'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': os.environ.get('DATABASE_PORT'),
+        'NAME': os.environ.get('DATABASE'),
+        'USER': os.environ.get('USER'),
+        'PASSWORD': os.environ.get('PASSWORD'),
+        'HOST': os.environ.get('HOST'),
+        'PORT': os.environ.get('PORT'),
     }
 }
 
